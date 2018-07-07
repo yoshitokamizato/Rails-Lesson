72ba4c569d12c248fe6d05532a7b8d0b4de3c4a3
Yoshito Kamizato <yuyueureka157@gmail.com>
2018-07-07 17:33:07 +0900

    add bootstrap code

--- app/controllers/rubys_controller.rb
+++ app/controllers/rubys_controller.rb
@@ -1,4 +1,3 @@
 class RubysController < ApplicationController
-  def index
-  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :rubys
   get '/puts', to: 'rubys#puts'
   get '/if', to: 'rubys#if'
+  get '/bootstrap', to: 'rails#bootstrap'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
