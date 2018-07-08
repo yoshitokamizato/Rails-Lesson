fefb5fe20fafda14c34013dbb08356cb0408dd01
Yoshito Kamizato <yuyueureka157@gmail.com>
2018-07-08 15:11:55 +0900

    add creation of environment

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get '/puts', to: 'rubys#puts'
   get '/if', to: 'rubys#if'
   get '/bootstrap', to: 'rails#bootstrap'
+  get '/environment', to: 'rails#environment'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
